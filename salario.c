4bed4f386c97e49c73dd719e1e0a41af169415c2
Renato Augusto <107155217+Argustor@users.noreply.github.com>
2023-02-23 20:13:01 -0300

    Update salario.c

--- salario.c
+++ salario.c
@@ -1,30 +1,29 @@
 //Faça um algoritmo para calcular o novo salário de um funcionário. Sabe-se que os funcionários que possuem salário até 500,00 terão aumento de 20%, os demais terão aumento de 10%
 
-#include <stdio.h>
 #include <stdlib.h>
 
 int main(){
 //Variáveis
 	char nome[100];
-	float s, ns, per;
+	float salario, novoSalario, percent;
 
 //Recebe os valores
 	printf("Digite o nome do funcionário:\n");
 	gets(nome);
 
 	printf("Digite o salário atual de %s:", nome);
-	scanf("%f",&s);
+	scanf("%f",&salario);
 
 //Condições
 
-if (s <= 500.00){
-	per = 20.0 / 100.0;//Cálculo da porcentagem
-	ns = s + (per * s);//Cálculo do novo salário
-		printf("O novo salário de %s é de %f:\n", nome, ns);//Resultado
+if (salario <= 500.00){
+	percent = 20.0 / 100.0;//Cálculo da porcentagem
+	novoSalario = salario + (percent * salario);//Cálculo do novo salário
+		printf("O novo salário de %s é de %.2f:\n", nome, novoSalario);//Resultado
 	}
 	else
-		per = 10.0 / 100.0;//Cálculo da porcentagem
-		ns = s + (per * s);//Cálculo do novo salário
-			printf("O novo salário de %s é de %f:\n", nome, ns);//Resultado
+		percent = 10.0 / 100.0;//Cálculo da porcentagem
+		novoSalario = salario + (percent * salario);//Cálculo do novo salário
+			printf("O novo salário de %s é de %.2f:\n", nome, novoSalario);//Resultado
 
 }
