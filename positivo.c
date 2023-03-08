6d738e5a6d63387597295860e4216ba4f75a2f52
Renato Augusto <107155217+Argustor@users.noreply.github.com>
2023-03-08 20:19:05 -0300

    Update positivo.c

--- positivo.c
+++ positivo.c
@@ -6,7 +6,7 @@ int main(){
 
 	int z = 0, x = 7, i, n = 1000; 
 
-	printf("\n\nEis os múltiplos de 7 inferiores a 1000.\n\n");
+	printf("\n\múltiplos de 7 inferiores a 1000.\n\n");
 
 	while (z < n){
 	i = x * z;
