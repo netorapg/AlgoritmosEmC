2203b5466c378e2a14235a5207ad569b378d6b8d
Argustor <renatoplatz11@outlook.com>
2023-08-20 17:24:05 -0300

    transformado tudo em funções

--- desafio/codigoDesafio.c
+++ desafio/codigoDesafio.c
@@ -3,8 +3,8 @@
 #include <math.h>
 #include <time.h>
 
+// Definição do tamanho máximo de estudantes
 #define NUM_ESTUDANTES 100
-
 
 // Estruct para armazenar os dados de um estudante
 typedef struct {
@@ -84,8 +84,6 @@ void insertionSort(int arr[], int n) {
     }
 }
 
-
-
 int lerEstudantesDoArquivo(const char *nomeArquivo, Estudante estudantes[NUM_ESTUDANTES]) {
     FILE *arquivo = fopen(nomeArquivo, "r");
     if (!arquivo) {
@@ -126,93 +124,50 @@ int escreverEstudantesNoArquivo(const char *nomeArquivo, Estudante estudantes[NU
     return 1; // Retornar verdadeiro indicando sucesso
 }
 
-// Fim das Funções
-int main () {
-    // Inicio do cronometro principal
-    clock_t inicioTotal, fimTotal;
-    double tempoGastoTotal;
-    inicioTotal = clock();
-
-    // Inicio do cronometro de leitura
-    clock_t inicioLeitura, fimLeitura;
-    inicioLeitura = clock();
-
-    Estudante estudantes[NUM_ESTUDANTES];
-    int numEstudantesLidos = lerEstudantesDoArquivo("arquivo.txt", estudantes);
-    
-    if (numEstudantesLidos == -1) {
-        return 1; // Houve erro ao ler o arquivo
-    }
-    // Fim do cronometro de leitura
-    fimLeitura = clock();
-    tempoGastoTotal = (double)(fimLeitura - inicioLeitura) / CLOCKS_PER_SEC;
-    printf ("Tempo gasto na leitura: %5f segundos\n", tempoGastoTotal);
-    // Inicio do cronometro de ordenação
-    clock_t inicioOrdenacao, fimOrdenacao;
-    inicioOrdenacao = clock();
-    // Ordena os estudantes
-    ordenarPorAnoInsertion(estudantes, NUM_ESTUDANTES);
-    // Fim do cronometro de ordenação
-    fimOrdenacao = clock();
-    tempoGastoTotal = (double)(fimOrdenacao - inicioOrdenacao) / CLOCKS_PER_SEC;
-    printf ("Tempo gasto na ordenação: %5f segundos\n", tempoGastoTotal);
-    // Inicio do cronometro de escrita
-    clock_t inicioEscrita, fimEscrita;
-    double tempoGastoEscrita;
-    inicioEscrita = clock();
-
-    if (!escreverEstudantesNoArquivo("notas.txt", estudantes)) {
-        return 1; // Houve erro ao escrever no arquivo
-    }
-    // Fim do cronometro de escrita
-    fimEscrita = clock();
-    tempoGastoEscrita = (double)(fimEscrita - inicioEscrita) / CLOCKS_PER_SEC;
-    printf ("Tempo gasto na escrita: %5f segundos\n", tempoGastoEscrita);
-    printf("\n");
-
-    // Coleta e impressão da media, mediana e desvio padrão
-    //das disciplinas por ano
+void imprimirEstatisticas(Estudante estudantes[NUM_ESTUDANTES]) {
+    // Coleta e impressão da media, mediana e desvio padrão das disciplinas por ano
     for (int ano = 2020; ano <= 2023; ano++) {
         printf("Ano: %d\n", ano);
         for (int disciplina = 0; disciplina < 5; disciplina++) {
-            int notas [100];
+            int notas[100];
             int n = 0;
 
             for (int i = 0; i < 100; i++) {
                 if (estudantes[i].ano == ano) {
                     notas[n++] = estudantes[i].notas[disciplina];
-               
                 }
             }
 
-           // qsort(notas, n, sizeof(int), compara);
-             insertionSort(notas, n);
+            insertionSort(notas, n);
             printf("Disciplina %d - Média: %.2f, Mediana: %.2f, Desvio Padrão: %.2f\n",
                    disciplina+1, media(notas, n), mediana(notas, n), desvioPadrao(notas, n));
         }
         printf("\n");
     }
+
     // Coleta e impressão da media, mediana e desvio padrão das disciplinas
-    printf ("Estatísticas por Disciplina (todos os anos): \n");
+    printf("Estatísticas por Disciplina (todos os anos):\n");
     for (int disciplina = 0; disciplina < 5; disciplina++) {
-        int notas [100];
+        int notas[100];
         int n = 0;
 
         for (int i = 0; i < 100; i++) {
             notas[n++] = estudantes[i].notas[disciplina];
         }
 
-        //qsort(notas, n, sizeof(int), compara);
         insertionSort(notas, n);
         printf("Disciplina %d - Média: %.2f, Mediana: %.2f, Desvio Padrão: %.2f\n",
                disciplina+1, media(notas, n), mediana(notas, n), desvioPadrao(notas, n));
     }
-    printf ("\n");
-   // Impressão dos resultados de aprovados e reprovados por disciplina
-    int aprovados [5] = {0};
-    int reprovados [5] = {0};
+    printf("\n");
+}
 
-    for (int i = 0; i < 100; i++){
+void imprimirResultados(Estudante estudantes[NUM_ESTUDANTES]) {
+    // Impressão dos resultados de aprovados e reprovados por disciplina
+    int aprovados[5] = {0};
+    int reprovados[5] = {0};
+
+    for (int i = 0; i < 100; i++) {
         for (int j = 0; j < 5; j++) {
             if (estudantes[i].notas[j] >= 70) {
                 aprovados[j]++;
@@ -221,13 +176,16 @@ int main () {
             }
         }
     }
-    printf ("Resultados:\n");
+
+    printf("Resultados:\n");
     printf("Total de alunos: 100\n");
     for (int j = 0; j < 5; j++) {
         printf("Disciplina %d - Aprovados: %d, Reprovados: %d\n", j+1, aprovados[j], reprovados[j]);
     }
     printf("\n");
+}
 
+void imprimirMelhorEPiorAluno(Estudante estudantes[NUM_ESTUDANTES]) {
     // Impressão do melhor e pior aluno
     Estudante *melhorAluno = NULL;
     Estudante *piorAluno = NULL;
@@ -252,7 +210,157 @@ int main () {
     if (piorAluno) {
         printf("Pior aluno: %d - Média: %.2f\n", piorAluno->id, piorMedia);
     }
+}
 
+// Fim das Funções
+int main () {
+    // Inicio do cronometro principal
+    clock_t inicioTotal, fimTotal;
+    double tempoGastoTotal;
+    inicioTotal = clock();
+
+    // Inicio do cronometro de leitura
+    clock_t inicioLeitura, fimLeitura;
+    inicioLeitura = clock();
+
+    // Declaração da variavel estudantes do tipo Estudante com NUM_ESTUDANTES posições
+    Estudante estudantes[NUM_ESTUDANTES];
+
+    // Ler os estudantes do arquivo
+    int numEstudantesLidos = lerEstudantesDoArquivo("arquivo.txt", estudantes);
+    
+    if (numEstudantesLidos == -1) {
+        return 1; // Houve erro ao ler o arquivo
+    }
+    // Fim do cronometro de leitura
+    fimLeitura = clock();
+    tempoGastoTotal = (double)(fimLeitura - inicioLeitura) / CLOCKS_PER_SEC;
+    printf ("Tempo gasto na leitura: %5f segundos\n", tempoGastoTotal);
+    // Inicio do cronometro de ordenação
+
+    clock_t inicioOrdenacao, fimOrdenacao;
+    inicioOrdenacao = clock();
+    // Ordena os estudantes
+    ordenarPorAnoInsertion(estudantes, NUM_ESTUDANTES);
+    // Fim do cronometro de ordenação
+    fimOrdenacao = clock();
+    tempoGastoTotal = (double)(fimOrdenacao - inicioOrdenacao) / CLOCKS_PER_SEC;
+    printf ("Tempo gasto na ordenação: %5f segundos\n", tempoGastoTotal);
+
+    // Inicio do cronometro de escrita
+    clock_t inicioEscrita, fimEscrita;
+    double tempoGastoEscrita;
+    inicioEscrita = clock();
+
+    if (!escreverEstudantesNoArquivo("notas.txt", estudantes)) {
+        return 1; // Houve erro ao escrever no arquivo
+    }
+    // Fim do cronometro de escrita
+    fimEscrita = clock();
+    tempoGastoEscrita = (double)(fimEscrita - inicioEscrita) / CLOCKS_PER_SEC;
+    printf ("Tempo gasto na escrita: %5f segundos\n", tempoGastoEscrita);
+    printf("\n");
+
+    // Coleta e impressão da media, mediana e desvio padrão
+    //das disciplinas por ano
+    clock_t inicioEstatisticas, fimEstatisticas;
+    inicioEstatisticas = clock();
+    imprimirEstatisticas(estudantes);
+    fimEstatisticas = clock();
+    tempoGastoTotal = (double)(fimEstatisticas - inicioEstatisticas) / CLOCKS_PER_SEC;
+    printf ("Tempo gasto na coleta e impressão das estatísticas: %5f segundos\n", tempoGastoTotal);
+    printf("\n");
+    /*for (int ano = 2020; ano <= 2023; ano++) {
+        printf("Ano: %d\n", ano);
+        for (int disciplina = 0; disciplina < 5; disciplina++) {
+            int notas [100];
+            int n = 0;
+
+            for (int i = 0; i < 100; i++) {
+                if (estudantes[i].ano == ano) {
+                    notas[n++] = estudantes[i].notas[disciplina];
+               
+                }
+            }
+             insertionSort(notas, n);
+            printf("Disciplina %d - Média: %.2f, Mediana: %.2f, Desvio Padrão: %.2f\n",
+                   disciplina+1, media(notas, n), mediana(notas, n), desvioPadrao(notas, n));
+        }
+        printf("\n");
+    }
+    // Coleta e impressão da media, mediana e desvio padrão das disciplinas
+    printf ("Estatísticas por Disciplina (todos os anos): \n");
+    for (int disciplina = 0; disciplina < 5; disciplina++) {
+        int notas [100];
+        int n = 0;
+
+        for (int i = 0; i < 100; i++) {
+            notas[n++] = estudantes[i].notas[disciplina];
+        }
+
+        insertionSort(notas, n);
+        printf("Disciplina %d - Média: %.2f, Mediana: %.2f, Desvio Padrão: %.2f\n",
+               disciplina+1, media(notas, n), mediana(notas, n), desvioPadrao(notas, n));
+    }
+    printf ("\n");*/
+    clock_t inicioResultado, fimResultado;
+    inicioResultado = clock();
+    imprimirResultados(estudantes);
+    fimResultado = clock();
+    tempoGastoTotal = (double)(fimResultado - inicioResultado) / CLOCKS_PER_SEC;
+
+    clock_t inicioMelhorPior, fimMelhorPior;
+    inicioMelhorPior = clock();
+    imprimirMelhorEPiorAluno(estudantes);
+    fimMelhorPior = clock();
+    tempoGastoTotal = (double)(fimMelhorPior - inicioMelhorPior) / CLOCKS_PER_SEC;
+    printf ("Tempo gasto na coleta e impressão dos resultados: %5f segundos\n", tempoGastoTotal);
+    printf("\n");
+    /*   // Impressão dos resultados de aprovados e reprovados por disciplina
+    int aprovados [5] = {0};
+    int reprovados [5] = {0};
+
+    for (int i = 0; i < 100; i++){
+        for (int j = 0; j < 5; j++) {
+            if (estudantes[i].notas[j] >= 70) {
+                aprovados[j]++;
+            } else {
+                reprovados[j]++;
+            }
+        }
+    }
+    printf ("Resultados:\n");
+    printf("Total de alunos: 100\n");
+    for (int j = 0; j < 5; j++) {
+        printf("Disciplina %d - Aprovados: %d, Reprovados: %d\n", j+1, aprovados[j], reprovados[j]);
+    }
+    printf("\n");*/
+
+    /*// Impressão do melhor e pior aluno
+    Estudante *melhorAluno = NULL;
+    Estudante *piorAluno = NULL;
+    double melhorMedia = -1.0;
+    double piorMedia = 100.0;
+
+    for (int i = 0; i < 100; i++) {
+        double mediaAtual = alunoMedia(&estudantes[i]);
+        if (mediaAtual > melhorMedia) {
+            melhorMedia = mediaAtual;
+            melhorAluno = &estudantes[i];
+        }
+        if (mediaAtual < piorMedia) {
+            piorMedia = mediaAtual;
+            piorAluno = &estudantes[i];
+        }
+    }
+
+    if (melhorAluno) {
+        printf("Melhor aluno: %d - Média: %.2f\n", melhorAluno->id, melhorMedia);
+    }
+    if (piorAluno) {
+        printf("Pior aluno: %d - Média: %.2f\n", piorAluno->id, piorMedia);
+    }
+*/
     // Fim do cronometro total
     fimTotal = clock();
     tempoGastoTotal = ((double)(fimTotal - inicioTotal)) / CLOCKS_PER_SEC;
